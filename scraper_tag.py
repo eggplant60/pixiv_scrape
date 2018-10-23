c600529e425f0235c192e02d82c08c04f4196998
eggplant60 <nagoyan240@gmail.com>
2018-10-23 17:00:07 +0900

    [update] download 済みの場合はスキップする処理を追加、try-except 内の処理の最小化

--- scraper_tag.py
+++ scraper_tag.py
@@ -6,6 +6,7 @@ from time import sleep
 import os
 import sys
 from password import *
+import glob
 
 
 # ログイン処理
@@ -20,7 +21,8 @@ if __name__ == '__main__':
 
     api = loginPixiv()
 
-    MAX_PAGE = 100
+    START_PAGE = 1
+    MAX_PAGE = 300
     PER_PAGE = 100
 
     # 閲覧数のしきい値
@@ -39,36 +41,54 @@ if __name__ == '__main__':
 
     separator = '------------------------------------------------------------'
     dl_cnt = 0
-    for page in range(1,MAX_PAGE):
+    for page in range(START_PAGE, MAX_PAGE+1):
+
         json_result = api.search_works(sys.argv[1], page=page, per_page=PER_PAGE,
                                        mode='exact_tag', types=['illustration'])
+        sleep(1)
+
         try:
-            for i in range(PER_PAGE):
-                #print(separator)
-                info = json_result.response[i]
+            N = len(json_result.response)
+            print(N)
+        except:
+            print('no result')
+            continue
             
-                if info.stats.views_count < VIEWS_CNT:
-                    continue
-                if info.stats.score < ARTIST_SCR:
-                    continue
+        for i in range(1,N+1):
             
-                print(separator)
-                print('{}/{}'.format((page-1)*PER_PAGE+i, MAX_PAGE*PER_PAGE-1))
-                print('    {} origin url: {}'.format(info.title, info.image_urls['large']))
-                print('    Artist: {}, Score: {}'.format(info.user.name, info.stats.score))
+            info = json_result.response[i-1]
             
-                # download
+
+            if info.stats.views_count < VIEWS_CNT:
+                continue
+            if info.stats.score < ARTIST_SCR:
+                continue
+
+            print(separator)
+            print('{}/{}'.format((page-1)*PER_PAGE+i, MAX_PAGE*PER_PAGE))
+            print('    {} origin url: {}'.format(info.title, info.image_urls['large']))
+            print('    Artist: {}, Score: {}'.format(info.user.name, info.stats.score))
+
+            # すでにダウンロード済みの場合はスキップ
+            
+            w_card = '????_????_' + str(info.id) + '_p0.*'
+            if glob.glob(os.path.join(saving_direcory_path, w_card)):
+                #print(w_card)
+                continue
+
+            try:
+                prefix = str(page).zfill(4)+'_'+str(i).zfill(4)+'_'
                 aapi.download(info.image_urls.large,
-                              prefix=str(page).zfill(4)+'_'+str(i).zfill(4)+'_',
+                              prefix=prefix,
                               path=saving_direcory_path)
                 dl_cnt += 1
                 sleep(1)
 
-        except:
-            print("Re-Login")
-            sleep(120)
-            api = loginPixiv()
-            continue
+            except:
+                print("Re-Login")
+                sleep(100)
+                api = loginPixiv()
+                continue
     
     print("\nCollected {} images.".format(dl_cnt))           
     print("That\'s all.")
